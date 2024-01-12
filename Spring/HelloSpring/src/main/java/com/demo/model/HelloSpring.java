aaf653c12aa4633d1667388e8cbdecfa86e46092
Nilesh-Parit <nileshparit4202@gmail.com>
2024-01-12 09:56:28 +0530

    SpringMVC

---
+++ Spring/HelloSpring/src/main/java/com/demo/model/HelloSpring.java
@@ -0,0 +1,12 @@
+package com.demo.model;
+
+public class HelloSpring
+{
+	public HelloSpring() {
+		System.out.println(" default constructor");
+		
+	}
+	public String sayHello() {
+		return "Hello Spring!!";
+	}
+}
